730f665ae955d716a9d130182324b47776c3a5ff
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 17:24:40 +0100

    functions
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -5,37 +5,28 @@ import (
 )
 
 var pl = fmt.Println
+
+func getSum(x int, y int) int {
+	return x + y
+}
+
+func getTwo(x int) (int, int) {
+	return x + 1, x + 2
+}
+
+func getQuotient(x float64, y float64) (ans float64, err error) {
+	if y == 0 {
+		return 0, fmt.Errorf("You can't divide by zero")
+	} else {
+		return x / y, nil
+	}
+}
+
 func main() {
-	// var name []datatype
-	sl1 := make([]string, 6)
-	sl1[0] = "Society"
-	sl1[1] = "of"
-	sl1[2] = "the"
-	sl1[3] = "Simulated"
-	sl1[4] = "Universe"
-	pl("Slice Size :", len(sl1))
-	for i := 0; i < len(sl1); i++ {
-		pl(sl1[i])
-	}
-	for _, x := range sl1 {
-		pl(x)
-	}
-	sArr := [5]int{1,2,3,4,5}
-	sl3 := sArr[0:2]
-	pl("1st 3 :", sArr[:3])
-	pl("Last 3 :", sArr[2:])
-	sArr[0] = 10
-	pl("sl3 :", sl3)
-	pl("sArr :", sArr)
-	sl3[0] = 1
-	pl("sArr :", sArr)
+	// func funcName(parameters) returnType {BODY}
 
-	pl("---")
-	sl3 = append(sl3, 12)
-	pl("sl3 :", sl3)
-	pl("sArr :", sArr)
-
-	sl4 := make([]string, 6)
-	pl("sl4 :", sl4)
-	pl("sl4[0] :", sl4[0])
+	pl(getSum(5, 4))
+	pl(getTwo(2))
+	pl(getQuotient(5, 0))
+	pl(getQuotient(5, 4))
 }
