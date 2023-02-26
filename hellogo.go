f4acec157aab62c2b599ab81e11ce4af9a6b6f60
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 16:04:32 +0100

    strings pkg, basic functions
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 var pl = fmt.Println
 func main() {
-	// Conditional Operators:  > < >= <= == !=
-	// Logical Operators:  && || !
+	sV1 := "A word"
+	replacer := strings.NewReplacer("A", "Another")
+	sV2 := replacer.Replace(sV1)
+	pl(sV2)
 
-	iAge := 8
-	if (iAge >= 1) && (iAge <= 18){
-		pl("Important Birthday")
-	} else if (iAge == 21) || (iAge == 50){
-		pl("Important Birthday")
-	} else if iAge >= 65 {
-		pl("Important Birthday")
-	} else {
-		pl("Not an Important Birthday")
-	}
+	pl("Length :", len(sV2))
+	pl("Contains Another :", strings.Contains(sV2, "Another"))
+	pl("o index :", strings.Index(sV2, "o"))
+	pl("Replace :", strings.Replace(sV2, "o", "0", -1))
+
+	sV3 := "\nSome Words\n"
+	pl(sV3)
+
+	sV3 = strings.TrimSpace(sV3)
+	pl(sV3)
+
+	pl("Split :", strings.Split("a-b-c-d", "-"))
+	pl("Lower :", strings.ToLower(sV2))
+	pl("Upper :", strings.ToUpper(sV2))
+	pl("Prefix :", strings.HasPrefix("tacocat", "taco"))
+	pl("Suffix :", strings.HasSuffix("tacocat", "cat"))
 }
