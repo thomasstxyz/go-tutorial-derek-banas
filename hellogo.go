455c2f9477a5e334c599672b227ebed5b7d8f4ad
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 17:26:57 +0100

    Variadic Functions
    receive unknown number of values
    
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,27 +6,14 @@ import (
 
 var pl = fmt.Println
 
-func getSum(x int, y int) int {
-	return x + y
-}
-
-func getTwo(x int) (int, int) {
-	return x + 1, x + 2
-}
-
-func getQuotient(x float64, y float64) (ans float64, err error) {
-	if y == 0 {
-		return 0, fmt.Errorf("You can't divide by zero")
-	} else {
-		return x / y, nil
+func getSum2(nums ...int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
 	}
+	return sum
 }
 
 func main() {
-	// func funcName(parameters) returnType {BODY}
-
-	pl(getSum(5, 4))
-	pl(getTwo(2))
-	pl(getQuotient(5, 0))
-	pl(getQuotient(5, 4))
+	pl(getSum2(1,2,3,4,44,3,2,25,2))
 }
