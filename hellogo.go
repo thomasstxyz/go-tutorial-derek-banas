74320376aa7fd3be0f89c6cb2342fd917c57979a
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 16:58:03 +0100

    while true loop, guessing game
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var pl = fmt.Println
 func main() {
-	// for loop
-	// for initialization; condition; postStatement {BODY}
-	for x := 1; x <= 5; x++ {
-		pl(x)
-	}
+	// while true loop, guessing game
 
-	// while loop
-	fX := 0
-	for fX < 5 {
-		pl(fX)
-		fX++
+	seedSecs := time.Now().Unix()
+	rand.Seed(seedSecs)
+	randNum := rand.Intn(50) + 1
+	for true {
+		fmt.Print("Guess a number between 0 and 50 :")
+		pl("Random Number is : ", randNum)
+		reader := bufio.NewReader(os.Stdin)
+		guess, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		guess = strings.TrimSpace(guess)
+		iGuess, err := strconv.Atoi(guess)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if iGuess > randNum {
+			pl("Pick a lower value!")
+		} else if iGuess < randNum {
+			pl("Pick a higher value!")
+		} else {
+			pl("You guessed it!")
+			break
+		}
 	}
 }
