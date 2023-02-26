064b13acbf97a4f661d5c149a3272d55b0d2cd49
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 16:49:06 +0100

    for and while loop
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,25 +6,16 @@ import (
 
 var pl = fmt.Println
 func main() {
-	// %d : Integer
-	// %c : Character
-	// %f : Float
-	// %t : Boolean
-	// %s : String
-	// %o : Base 8
-	// %x : Base 16
-	// %v : Guesses based on data type
-	// %T : Type of supplied value
+	// for loop
+	// for initialization; condition; postStatement {BODY}
+	for x := 1; x <= 5; x++ {
+		pl(x)
+	}
 
-	fmt.Printf("%s %d %c %f %t %o %x\n",
-		"Stuff", 1, 'A', 3.14, true, 1, 1)
-
-	fmt.Printf("%9f\n", 3.14)
-	fmt.Printf("%9.2f\n", 3.141592)
-	fmt.Printf("%9.f\n", 3.141592)
-	fmt.Printf("\n")
-
-	sp1 := fmt.Sprintf("%9.f\n", 3.141592)
-	fmt.Printf("%9s\n", "---")
-	fmt.Println(sp1)
+	// while loop
+	fX := 0
+	for fX < 5 {
+		pl(fX)
+		fX++
+	}
 }
