aa3f08658eff70f3826bb40146fcbc914fbd99b9
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 22:06:12 +0100

    interfaces
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,5 +6,29 @@ import (
 
 var pl = fmt.Println
 
+type Animal interface{
+	AngrySound()
+	HappySound()
+}
+type Cat string
+func (c Cat) Attack(){
+	pl("cat attacks its prey")
+}
+func (c Cat) Name() string{
+	return string(c)
+}
+func (c Cat) AngrySound(){
+	pl("Cat says Hisssss")
+}
+func (c Cat) HappySound(){
+	pl("Cat says Purrrrr")
+}
+
 func main() {
+	var kitty Animal
+	kitty = Cat("Kitty")
+	kitty.AngrySound()
+	var kitty2 Cat = kitty.(Cat)
+	kitty2.Attack()
+	pl("Cats Name :", kitty2.Name())
 }
