9c00b1c1c41623cb7284ea29d83c1d4a6e014ddd
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 20:20:04 +0100

    slices, float64 and function
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,14 +6,16 @@ import (
 
 var pl = fmt.Println
 
-func dblArrVals(arr *[4]int){
-	for x := 0; x < 4; x++ {
-		arr[x] *= 2
+func getAverage(nums ...float64) float64 {
+	var sum float64 = 0.0
+	var numSize float64 = float64(len(nums))
+	for _, val := range nums {
+		sum += val
 	}
+	return (sum / numSize)
 }
 
 func main() {
-	pArr := [4]int{1,2,3,4}
-	dblArrVals(&pArr)
-	pl(pArr)
+	iSlice := []float64{11, 13, 17}
+	fmt.Printf("Average : %.3f\n", getAverage(iSlice...))
 }
