df46e6df95e508c60199754fc294dfb43c75bc40
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 17:09:52 +0100

    rune array and byte array
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,25 +6,12 @@ import (
 
 var pl = fmt.Println
 func main() {
-	var arr1 [5]int
-	arr1[0] = 1
-
-	arr2 := [5]int{1,2,3,4,5}
-	pl("Index 0 :", arr2[0])
-	pl("Arr Length :", len(arr2))
-	for i := 0; i < len(arr2); i++ {
-		pl(arr2[i])
+	aStr1 := "abcde"
+	rArr := []rune(aStr1)
+	for _, v := range rArr {
+		fmt.Printf("Rune Array : %d\n", v)
 	}
-	for i, v := range arr2 {
-		fmt.Printf("%d : %d\n", i, v)
-	}
-	arr3 := [2][2]int{
-		{1,2},
-		{3,4},
-	}
-	for i := 0; i < 2; i++ {
-		for j := 0; j < 2; j++ {
-			pl(arr3[i][j])
-		}
-	}
+	byteArr := []byte{'a', 'b', 'c'}
+	bStr := string(byteArr[:])
+	pl("I'm a string :", bStr)
 }
