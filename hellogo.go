d18da874886ae16504690435a840d417dd32454c
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 21:26:51 +0100

    structs
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,15 +6,27 @@ import (
 
 var pl = fmt.Println
 
-type MyConstraint interface {
-	int | float64
+type customer struct {
+	name string
+	address string
+	bal float64
 }
 
-func getSumGen[T MyConstraint](x T, y T) T {
-	return x + y
+func getCustInfo(c customer){
+	fmt.Printf("%s owes us %.2f\n", c.name, c.bal)
+}
+func newCustAdd(c *customer, address string){
+	c.address = address
 }
 
 func main() {
-	pl("5 + 4 =", getSumGen(5,4))
-	pl("5.6 + 4.7 =", getSumGen(5.6,4.7))
+	var tS customer
+	tS.name = "Tom Smith"
+	tS.address = "5 main st"
+	tS.bal = 234.56
+	getCustInfo(tS)
+	newCustAdd(&tS, "123 South st")
+	pl("Address :", tS.address)
+	sS := customer{"Sally Smith", "123 Main", 0.0}
+	pl("Name :", sS.name)
 }
