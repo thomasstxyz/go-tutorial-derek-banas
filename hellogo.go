b87cef0aa85698b7d6e92c2918308f13057651f2
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 15:55:11 +0100

    Conditional Operators:  > < >= <= == != Logical Operators:  && || !
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -2,29 +2,21 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"strconv"
 )
 
 var pl = fmt.Println
 func main() {
-	cV1 := 1.5
-	cV2 := int(cV1)
-	pl(cV2)
+	// Conditional Operators:  > < >= <= == !=
+	// Logical Operators:  && || !
 
-	cV3 := "50000000"
-	cV4, err := strconv.Atoi(cV3)
-	pl(cV4, err, reflect.TypeOf(cV4))
-
-	cV5 := 50000000
-	cV6 := strconv.Itoa(cV5)
-	pl(cV6)
-
-	cV7 := "3.14"
-	if cV8, err := strconv.ParseFloat(cV7, 64); err == nil{
-		pl(cV8)
+	iAge := 8
+	if (iAge >= 1) && (iAge <= 18){
+		pl("Important Birthday")
+	} else if (iAge == 21) || (iAge == 50){
+		pl("Important Birthday")
+	} else if iAge >= 65 {
+		pl("Important Birthday")
+	} else {
+		pl("Not an Important Birthday")
 	}
-
-	cV9 := fmt.Sprintf("%f", 3.14)
-	fmt.Println(cV9)
 }
