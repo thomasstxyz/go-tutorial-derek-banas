e2cc72945336fbf5520f7ee59b835044a50ec4a3
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 17:18:30 +0100

    slices
    slice = view of an array
    
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,12 +6,36 @@ import (
 
 var pl = fmt.Println
 func main() {
-	aStr1 := "abcde"
-	rArr := []rune(aStr1)
-	for _, v := range rArr {
-		fmt.Printf("Rune Array : %d\n", v)
+	// var name []datatype
+	sl1 := make([]string, 6)
+	sl1[0] = "Society"
+	sl1[1] = "of"
+	sl1[2] = "the"
+	sl1[3] = "Simulated"
+	sl1[4] = "Universe"
+	pl("Slice Size :", len(sl1))
+	for i := 0; i < len(sl1); i++ {
+		pl(sl1[i])
 	}
-	byteArr := []byte{'a', 'b', 'c'}
-	bStr := string(byteArr[:])
-	pl("I'm a string :", bStr)
+	for _, x := range sl1 {
+		pl(x)
+	}
+	sArr := [5]int{1,2,3,4,5}
+	sl3 := sArr[0:2]
+	pl("1st 3 :", sArr[:3])
+	pl("Last 3 :", sArr[2:])
+	sArr[0] = 10
+	pl("sl3 :", sl3)
+	pl("sArr :", sArr)
+	sl3[0] = 1
+	pl("sArr :", sArr)
+
+	pl("---")
+	sl3 = append(sl3, 12)
+	pl("sl3 :", sl3)
+	pl("sArr :", sArr)
+
+	sl4 := make([]string, 6)
+	pl("sl4 :", sl4)
+	pl("sl4[0] :", sl4[0])
 }
