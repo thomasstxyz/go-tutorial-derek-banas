e0364f512a17195b9f32167ed085143e382d6ff7
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 21:21:07 +0100

    generics
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,24 +6,15 @@ import (
 
 var pl = fmt.Println
 
+type MyConstraint interface {
+	int | float64
+}
+
+func getSumGen[T MyConstraint](x T, y T) T {
+	return x + y
+}
+
 func main() {
-	// var myMap map [keyType]valueType
-	var heroes map[string]string
-	heroes = make(map[string]string)
-	villians := make(map[string]string)
-	heroes["Batman"] = "Bruce Wayne"
-	heroes["Superman"] = "Clark Kent"
-	heroes["The Flash"] = "Barry Allen"
-	villians["Lex Luther"] = "Lex Luther"
-
-	superPets := map[int]string{1: "Krypto", 2: "Bat Hound"}
-	fmt.Printf("Batman is %v\n", heroes["Batman"])
-	pl("Chip :", superPets[3])
-	_, ok := superPets[3]
-	pl("Is there a 3rd pet :", ok)
-	delete(heroes, "The Flash")
-	for k, v := range heroes {
-		fmt.Printf("%s is %s\n", k, v)
-	}
-
+	pl("5 + 4 =", getSumGen(5,4))
+	pl("5.6 + 4.7 =", getSumGen(5.6,4.7))
 }
