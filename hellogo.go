000d279eef897f86f222267d9d10ad63e04fbfa5
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 17:45:44 +0100

    more pointers to change values with functions
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,26 +6,20 @@ import (
 
 var pl = fmt.Println
 
-func changeVal(f3 int) int {
-	f3 += 1
-	return f3
-}
-
 func changeVal2(myPtr *int) int {
 	*myPtr = 12
 	return *myPtr
 }
 
 func main() {
-	f3 := 5
-	pl("f3 before func :", f3)
-	changeVal(f3)
-	pl("f3 after func :", f3)
-	pl()
+	f4 := 10
+	var f4Ptr *int = &f4
+	pl("f4 Address :", f4Ptr)
+	pl("f4 Value :", *f4Ptr)
+	*f4Ptr = 11
+	pl("f4 Value :", *f4Ptr)
 
-	f4 := 5
 	pl("f4 before func :", f4)
 	changeVal2(&f4)
 	pl("f4 after func :", f4)
-	pl()
 }
