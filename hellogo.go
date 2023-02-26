6d8c1fa0fd243ea4ec9cda744b9e4b9d0cc4cf49
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 21:10:32 +0100

    maps
    key value maps
    
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -7,4 +7,23 @@ import (
 var pl = fmt.Println
 
 func main() {
+	// var myMap map [keyType]valueType
+	var heroes map[string]string
+	heroes = make(map[string]string)
+	villians := make(map[string]string)
+	heroes["Batman"] = "Bruce Wayne"
+	heroes["Superman"] = "Clark Kent"
+	heroes["The Flash"] = "Barry Allen"
+	villians["Lex Luther"] = "Lex Luther"
+
+	superPets := map[int]string{1: "Krypto", 2: "Bat Hound"}
+	fmt.Printf("Batman is %v\n", heroes["Batman"])
+	pl("Chip :", superPets[3])
+	_, ok := superPets[3]
+	pl("Is there a 3rd pet :", ok)
+	delete(heroes, "The Flash")
+	for k, v := range heroes {
+		fmt.Printf("%s is %s\n", k, v)
+	}
+
 }
