6a4ec7c279a1dc8e535b1094bc18fc08c8a0840f
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 17:35:17 +0100

    pointers to change variable inside of function
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,17 +6,26 @@ import (
 
 var pl = fmt.Println
 
-func getArraySum(arr []int) int {
-	sum := 0
-	for _, val := range arr {
-		sum += val
-	}
-	return sum
+func changeVal(f3 int) int {
+	f3 += 1
+	return f3
+}
+
+func changeVal2(myPtr *int) int {
+	*myPtr = 12
+	return *myPtr
 }
 
 func main() {
-	vArr := []int{1,2,3,4}
-	pl("Array Sum :", getArraySum(vArr))
+	f3 := 5
+	pl("f3 before func :", f3)
+	changeVal(f3)
+	pl("f3 after func :", f3)
+	pl()
 
-	
+	f4 := 5
+	pl("f4 before func :", f4)
+	changeVal2(&f4)
+	pl("f4 after func :", f4)
+	pl()
 }
