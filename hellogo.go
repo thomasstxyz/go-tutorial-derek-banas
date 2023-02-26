2d8dbe3b80a2740a10d94bb1798fdbc4ff46cb3a
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 15:50:52 +0100

    casting, convert int to ascii, ..
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -3,16 +3,28 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 var pl = fmt.Println
 func main() {
-	//            int, float64,  bool, string, rune
-	// Default      0,     0.0, false,     "",   ""
+	cV1 := 1.5
+	cV2 := int(cV1)
+	pl(cV2)
 
-	pl(reflect.TypeOf(15))
-	pl(reflect.TypeOf(13.3))
-	pl(reflect.TypeOf(true))
-	pl(reflect.TypeOf("hello"))
-	pl(reflect.TypeOf('🦧'))
+	cV3 := "50000000"
+	cV4, err := strconv.Atoi(cV3)
+	pl(cV4, err, reflect.TypeOf(cV4))
+
+	cV5 := 50000000
+	cV6 := strconv.Itoa(cV5)
+	pl(cV6)
+
+	cV7 := "3.14"
+	if cV8, err := strconv.ParseFloat(cV7, 64); err == nil{
+		pl(cV8)
+	}
+
+	cV9 := fmt.Sprintf("%f", 3.14)
+	fmt.Println(cV9)
 }
