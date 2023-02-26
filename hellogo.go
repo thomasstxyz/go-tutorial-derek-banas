bceb460c8ad2633c2097ae5cba6eea8aff2ac00c
Thomas Stadler <thomas@thomasst.xyz>
2023-02-26 21:33:48 +0100

    functions,methods of structs
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,27 +6,15 @@ import (
 
 var pl = fmt.Println
 
-type customer struct {
-	name string
-	address string
-	bal float64
+type rectangle struct{
+	length, height float64
 }
 
-func getCustInfo(c customer){
-	fmt.Printf("%s owes us %.2f\n", c.name, c.bal)
-}
-func newCustAdd(c *customer, address string){
-	c.address = address
+func (r rectangle) Area() float64{
+	return r.length * r.height
 }
 
 func main() {
-	var tS customer
-	tS.name = "Tom Smith"
-	tS.address = "5 main st"
-	tS.bal = 234.56
-	getCustInfo(tS)
-	newCustAdd(&tS, "123 South st")
-	pl("Address :", tS.address)
-	sS := customer{"Sally Smith", "123 Main", 0.0}
-	pl("Name :", sS.name)
+	rect1 := rectangle{10.0, 15.0}
+	pl("Rectangle Area :", rect1.Area())
 }
