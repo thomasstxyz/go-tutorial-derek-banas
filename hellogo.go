4a74d1ab1cfc33a812a144abc4ef5aa30fea0f8f
Thomas Stadler <thomas@thomasst.xyz>
2023-02-27 22:25:10 +0100

    recursion
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -6,24 +6,13 @@ import (
 
 var pl = fmt.Println
 
-func useFunc(f func(int, int) int, x, y int) {
-	pl("Answer :", (f(x, y)))
-}
-
-func sumvals(x, y int) int {
-	return x + y
+func factorial(num int) int{
+	if num == 0 {
+		return 1
+	}
+	return num * factorial(num-1)
 }
 
 func main() {
-	intSum := func(x, y int) int { return x + y}
-	pl("5 + 4 =", intSum(5, 4))
-
-	samp1 := 1
-	changeVar := func(){
-		samp1 += 1
-	}
-	changeVar()
-	pl("samp1 =", samp1)
-
-	useFunc(sumvals, 5, 8)
+	pl("Factorial 4 =", factorial(4))
 }
