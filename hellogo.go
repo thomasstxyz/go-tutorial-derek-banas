fbf810b32fdb7486da9a343b97365201002b3812
Thomas Stadler <thomas@thomasst.xyz>
2023-02-27 22:14:59 +0100

    closures
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- hellogo.go
+++ hellogo.go
@@ -2,39 +2,18 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
 var pl = fmt.Println
 
-type Account struct{
-	balance int
-	lock sync.Mutex
-}
-func (a *Account) GetBalance() int {
-	a.lock.Lock()
-	defer a.lock.Unlock()
-	return a.balance
-}
-func (a *Account) Withdraw(v int){
-	a.lock.Lock()
-	defer a.lock.Unlock()
-	if v > a.balance {
-		pl("Not enough money in account")
-	} else {
-		fmt.Printf("%d withdrawn : Balance : %d\n",
-			v, a.balance)
-		a.balance -= v
-	}
-}
-
 func main() {
-	var acct Account
-	acct.balance = 100
-	pl("Balance :", acct.GetBalance())
-	for i := 0; i < 12; i++ {
-		go acct.Withdraw(10)
+	intSum := func(x, y int) int { return x + y}
+	pl("5 + 4 =", intSum(5, 4))
+
+	samp1 := 1
+	changeVar := func(){
+		samp1 += 1
 	}
-	time.Sleep(2 * time.Second)
+	changeVar()
+	pl("samp1 =", samp1)
 }
